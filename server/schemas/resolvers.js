36a8b4a5d09bafbe3577eddf2a03d27d83d49fa4
vlada_caban <vladaonly@gmail.com>
2023-09-02 23:22:13 -0400

    updated schema using context

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,31 +4,24 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 const resolvers = {
   Query: {
     //get a single user by either their id or their username
-    getSingleUser: async (_, { user = null, params }) => {
-      const foundUser = await User.findOne({
-        $or: [
-          { _id: user ? user._id : params.id },
-          { username: params.username },
-        ],
-      });
+    getSingleUser: async (_, args) => {
+      const foundUser = await User.findOne({ email: args.email });
       return foundUser;
     },
   },
   Mutation: {
     // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-    createUser: async (_, { body }) => {
-      const user = await User.create({ body });
+    createUser: async (_, args) => {
+      const user = await User.create(args);
       const token = signToken(user);
 
       return { token, user };
     },
 
     // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-    // {body} is destructured req.body
-    login: async (_, { body }) => {
-      const user = await User.findOne({
-        $or: [{ username: body.username }, { email: body.email }],
-      });
+    // {email, password} are destructured args
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email: email });
 
       if (!user) {
         throw AuthenticationError;
@@ -45,16 +38,34 @@ const resolvers = {
     },
 
     // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
-    // user comes from `req.user` created in the auth middleware function
-    saveBook: async (_, { user, body }) => {
-      console.log(user);
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
-        { new: true, runValidators: true }
-      );
+    // user comes from context 
+    saveBook: async (_, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.bookToSave } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } else {
+        throw AuthenticationError;
+      }
+    },
 
-      return updatedUser;
+    // remove a book from `savedBooks`
+    //user comes from context
+    deleteBook: async (_, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookToDelete } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      } else {
+        throw AuthenticationError;
+      }
     },
   },
 };
